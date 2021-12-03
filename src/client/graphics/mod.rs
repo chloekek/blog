5396e9cb3d7848a260f64be60fb8ea66c2edbe7b
Chloé Kekoa <chloekek@use.startmail.com>
2021-12-03 01:32:24 +0100

    Add draft of trivial block face pipeline

---
+++ src/client/graphics/mod.rs
@@ -0,0 +1,5 @@
+//! Graphics rendering pipelines.
+
+pub use self::trivial_block::*;
+
+mod trivial_block;
--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,3 +1,4 @@
 //! Logic that is specific to the client.
 
 pub mod input;
+pub mod graphics;
