0e5d9534d4ea1afce3c2897fb4c684927406b92b
Chloé Kekoa <chloekek@use.startmail.com>
2021-12-07 21:28:26 +0100

    Draft vertex shader for generic pipeline

--- blok/src/client/graphics/generic/mod.rs
+++ blok/src/client/graphics/generic/mod.rs
@@ -1,25 +1,55 @@
 pub use self::fragment_shader::*;
-pub use self::instance::*;
-pub use self::model::*;
 
 use anyhow::Result;
+use glam::{Mat4, Vec2, Vec3};
+use opengl::gl::types::*;
 use std::borrow::Borrow;
 
 mod fragment_shader;
-mod instance;
-mod model;
 
-pub struct Pipeline
+#[repr(C)]
+pub struct Vertex
+{
+    pub position: Vec3,
+    pub texcoord: Vec2,
+
+    /// Index of the bones to apply.
+    ///
+    /// At the moment exactly one bone is applied to each vertex.
+    /// To apply no bones, set the bone rotation and position to zero.
+    pub bone: u32,
+}
+
+pub struct Model<const BONES: usize>
+{
+    vertex_buffer: GLuint,
+    index_buffer: GLuint,
+}
+
+pub struct Instance<const BONES: usize>
+{
+    pub m_matrix: Mat4,
+    pub bone_matrices: [Mat4; BONES],
+}
+
+pub struct Pipeline<const BONES: usize>
 {
 }
 
-impl Pipeline
+impl<const BONES: usize> Pipeline<BONES>
 {
     #[doc = crate::doc_safety_opengl!()]
-    pub unsafe fn render<I, M, N>(models: I) -> Result<()>
-        where I: IntoIterator<Item=(M, N)>
-            , M: Borrow<Model>
-            , N: Borrow<InstanceSet>
+    pub unsafe fn new() -> Result<Self>
+    {
+        todo!()
+    }
+
+    #[doc = crate::doc_safety_opengl!()]
+    pub unsafe fn render<I, J, M, N>(models: I) -> Result<()>
+        where I: IntoIterator<Item=(M, J)>
+            , J: IntoIterator<Item=N>
+            , M: Borrow<Model<BONES>>
+            , N: Borrow<Instance<BONES>>
     {
         todo!()
     }
--- blok/src/client/graphics/generic/shader.vert
+++ blok/src/client/graphics/generic/shader.vert
@@ -1,5 +1,25 @@
 #version 450 core
 
+// This specialization constant is overridden during pipeline construction.
+// GLSL requires a default value and I picked 1 arbitrarily.
+layout(constant_id = 1) const uint BONES = 1;
+
+layout(location = 0) uniform mat4 mvp_matrix;
+layout(location = 1) uniform mat4 bone_matrices[BONES];
+
+layout(location = 0) in vec3 vertex_position;
+layout(location = 1) in vec2 vertex_texcoord;
+layout(location = 2) in uint vertex_bone;
+
+layout(location = 0) out vec2 fragment_uv;
+
 void main()
 {
+    gl_Position =
+        mvp_matrix *
+        bone_matrices[vertex_bone] *
+        vec4(vertex_position, 1.0);
+
+    // Pass through texture coordinate.
+    fragment_uv = vertex_texcoord;
 }
