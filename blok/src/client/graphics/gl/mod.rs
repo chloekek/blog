ce93a1a8e39a46d4155ea65d8c0f8fbcb3c16450
Chloé Kekoa <chloekek@use.startmail.com>
2021-12-08 00:54:06 +0100

    Move OpenGL utilities to separate folder

---
+++ blok/src/client/graphics/gl/mod.rs
@@ -0,0 +1,9 @@
+pub use self::gl_buffer::*;
+pub use self::gl_error::*;
+pub use self::gl_program::*;
+pub use self::gl_shader::*;
+
+mod gl_buffer;
+mod gl_error;
+mod gl_program;
+mod gl_shader;
--- blok/src/client/graphics/mod.rs
+++ blok/src/client/graphics/mod.rs
@@ -1,15 +1,9 @@
 //! Graphics rendering pipelines.
 
-pub use self::gl_buffer::*;
-pub use self::gl_error::*;
-pub use self::gl_program::*;
-pub use self::gl_shader::*;
+pub use self::gl::*;
 
 pub mod generic;
 pub mod parameters;
 pub mod trivial_block;
 
-mod gl_buffer;
-mod gl_error;
-mod gl_program;
-mod gl_shader;
+mod gl;
